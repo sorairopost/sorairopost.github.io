f81dcdb64df5e6cd034072d4d3191aea52914f23
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2022-10-21 00:39:08 +0900

    changed heroimages

---
+++ assets/index.html.518d9a36.js
@@ -0,0 +1,8 @@
+import{_ as e,o as a,c,d as n,a as t}from"./app.d413c5a6.js";const s={},o=t("div",{class:"custom-container tip"},[t("p",{class:"custom-container-title"},"TIP"),t("ul",null,[t("li",null,"\u3053\u3061\u3089\u306E\u30B5\u30A4\u30C8\u306F\u5DE5\u4E8B\u4E2D\u3067\u3059\uFF0E")]),n(" * [note\u306B\u3066SF\u30D7\u30ED\u30C8\u30BF\u30A4\u30D4\u30F3\u30B0\u6D3B\u52D5\u516C\u958B\u4E2D\uFF01](https://note.com/clean_plover315) ")],-1);function l(i,r){return a(),c("div",null,[n(` <div style="text-align:center">
+<p>\u3053\u3061\u3089\u306F\u9999\u5DDD\u5927\u5B66\u533B\u5B66\u90E8\u9644\u5C5E\u75C5\u9662\u306E\u4E2D\u5EAD\u306E\u7A7A\u306B\u6295\u51FD\u3055\u308C\u305F\u624B\u7D19\u306E\u8A18\u61B6\u30B5\u30A4\u30C8\u3067\u3059</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">\u624B\u7D19\u3092\u8AAD\u3080\u524D\u306B \u2192</a></p>
+</div> `),n(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">\u624B\u7D19\u3092\u8AAD\u3080 \u2192</a></p>
+</div> `),o])}const p=e(s,[["render",l],["__file","index.html.vue"]]);export{p as default};
