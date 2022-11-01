8490551580ad01a3350887cbdec7edb301173cfd
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2022-11-01 23:33:15 +0900

    Add picup.md

---
+++ assets/0035.23389af1.js
@@ -0,0 +1,1 @@
+const s="/assets/img/images/letter/0034.jpg",t="/assets/img/images/letter/0035.jpg";export{s as _,t as a};
---
+++ assets/index.html.21a433bd.js
@@ -0,0 +1,8 @@
+import{_ as n,o as a,c,e,b as t}from"./app.7ec834e7.js";const s={},o=t("div",{class:"custom-container tip"},[t("p",{class:"custom-container-title"},"TIP"),t("ul",null,[t("li",null,"まもなく10月31日までに投函されたお手紙が掲載されます．")]),e(" * [noteにてSFプロトタイピング活動公開中！](https://note.com/clean_plover315) ")],-1);function l(i,r){return a(),c("div",null,[e(` <div style="text-align:center">
+<p>こちらは香川大学医学部附属病院の中庭の空に投函された手紙の記憶サイトです</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">手紙を読む前に →</a></p>
+</div> `),e(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">手紙を読む →</a></p>
+</div> `),o])}const p=n(s,[["render",l],["__file","index.html.vue"]]);export{p as default};
