bdaa7b1bf2700c394460de3f1799b37a15dabe24
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2022-11-01 17:25:02 +0900

    Fixed Top Page Link

---
+++ assets/index.html.5518673a.js
@@ -0,0 +1,8 @@
+import{_ as e,o as a,c,b as n,d as t}from"./app.b75e5162.js";const s={},o=t("div",{class:"custom-container tip"},[t("p",{class:"custom-container-title"},"TIP"),t("ul",null,[t("li",null,"まもなく10月31日までに投函されたお手紙が掲載されます．")]),n(" * [noteにてSFプロトタイピング活動公開中！](https://note.com/clean_plover315) ")],-1);function l(i,r){return a(),c("div",null,[n(` <div style="text-align:center">
+<p>こちらは香川大学医学部附属病院の中庭の空に投函された手紙の記憶サイトです</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">手紙を読む前に →</a></p>
+</div> `),n(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">手紙を読む →</a></p>
+</div> `),o])}const p=e(s,[["render",l],["__file","index.html.vue"]]);export{p as default};
