f124a2dbae554ca56f5d66913c9881223639f552
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2022-11-03 17:45:26 +0900

    Changed color

---
+++ assets/index.html.f146b348.js
@@ -0,0 +1,8 @@
+import{_ as e,o as a,c,e as t,b as n}from"./app.231e52d5.js";const l={},s=n("div",{class:"custom-container tip"},[n("p",{class:"custom-container-title"},"TIP"),n("ul",null,[n("li",null,"まもなく11月02日までに投函されたお手紙が掲載されます．"),n("li",null,"スタッフが手作業で1枚1枚をスキャンして掲載しています．必ずすべての手紙を掲載しますが，掲載日が遅れる場合もあります．ご容赦くださいますと幸いです．")])],-1);function i(o,r){return a(),c("div",null,[t(` <div style="text-align:center">
+<p>こちらは香川大学医学部附属病院の中庭の空に投函された手紙の記憶サイトです</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">手紙を読む前に →</a></p>
+</div> `),t(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">手紙を読む →</a></p>
+</div> `),s])}const u=e(l,[["render",i],["__file","index.html.vue"]]);export{u as default};
