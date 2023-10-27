9dd2ec665835eb7633d8c0a2f99cab561f1eb94b
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2023-10-27 20:57:28 +0900

    Changed dark mode hero image

---
+++ assets/index.html.3c9d856e.js
@@ -0,0 +1,8 @@
+import{_ as e,o as l,c as a,b as t,d as n}from"./app.23143c3e.js";const c={},s=n("div",{class:"custom-container tip"},[n("p",{class:"custom-container-title"},"TIP"),n("ul",null,[n("li",null,"2023年度の空色ポストは2023年10月30日から11月2日まで開催されます．"),n("li",null,"現在，2022年11月04日までに投函されたお手紙のうち231枚が掲載されています．"),n("li",null,"スタッフが手作業で1枚1枚をスキャンして掲載しています．必ずすべての手紙を掲載しますが，掲載日が遅れる場合もあります．ご容赦くださいますと幸いです．")])],-1);function i(o,r){return l(),a("div",null,[t(` <div style="text-align:center">
+<p>こちらは香川大学医学部附属病院の中庭の空に投函された手紙の記憶サイトです</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">手紙を読む前に →</a></p>
+</div> `),t(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">手紙を読む →</a></p>
+</div> `),s])}const u=e(c,[["render",i],["__file","index.html.vue"]]);export{u as default};
