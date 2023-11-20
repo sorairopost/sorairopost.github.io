480477329a2e39173af02477704ab22062134fee
Takayoshi Kitamura <[ktmr@fc.ritsumei.ac.jp]>
2023-11-20 12:15:00 +0900

    Add comming_soon.md

---
+++ assets/index.html-f64482bf.js
@@ -0,0 +1,8 @@
+import{_ as e,o as l,c as a,e as t,b as n}from"./app-6690f11b.js";const c={},s=n("div",{class:"custom-container tip"},[n("p",{class:"custom-container-title"},"TIP"),n("ul",null,[n("li",null,"2023年度の空色ポストは2023年10月30日から11月2日まで開催されました．"),n("li",null,"2023年度に投函されたお手紙は現在のところ170枚が集まっています．"),n("li",null,"2022年度に投函されたお手紙の合計は231枚でした．"),n("li",null,"スタッフが手作業で1枚1枚をスキャンして確認後，掲載しています．必ずすべての手紙を掲載しますが，掲載が遅れる場合もあります．ご容赦くださいますと幸いです．")])],-1);function i(o,r){return l(),a("div",null,[t(` <div style="text-align:center">
+<p>こちらは香川大学医学部附属病院の中庭の空に投函された手紙の記憶サイトです</p>
+</div>
+<div class="hero">
+<p class="action"><a href="about.html" class="nav-link action-button">手紙を読む前に →</a></p>
+</div> `),t(` <div class="hero">
+<p class="action"><a href="picup.html" class="nav-link action-button">手紙を読む →</a></p>
+</div> `),s])}const d=e(c,[["render",i],["__file","index.html.vue"]]);export{d as default};
